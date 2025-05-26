25e7be95fef4de0a4cb908140d572ddea1a3bb36
Gengye <1094430606@qq.com>
2025-05-26 17:35:51 +0200

    Initial commit

---
+++ src/main/java/com/gamers/GamersApplication.java
@@ -0,0 +1,13 @@
+package com.gamers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamersApplication.class, args);
+	}
+
+}
